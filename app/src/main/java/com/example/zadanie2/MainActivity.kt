1d522139f48e5291a14fc647c6af4751a0d0ba59
Oskar <o_swiderski@o2.pl>
2021-11-10 08:33:49 +0100

    skonczone

--- app/src/main/java/com/example/zadanie2/MainActivity.kt
+++ app/src/main/java/com/example/zadanie2/MainActivity.kt
@@ -15,19 +15,25 @@ class MainActivity : AppCompatActivity() {
         val do_podania_imie = findViewById<TextInputEditText>(R.id.podane_imie)
         val do_podania_nazwisko = findViewById<TextInputEditText>(R.id.podane_nazwisko)
         val tekst_wynik = findViewById<TextView>(R.id.Wynik)
+        val dospr = findViewById<TextView>(R.id.dospr)
+        val dospr2 = findViewById<TextView>(R.id.dospr2)
 
-            if (wyslane != null && do_podania_imie != null  && do_podania_nazwisko != null)
+
+
+        wyslane.setOnClickListener {
+            dospr.text = do_podania_imie.text.toString()
+            dospr2.text = do_podania_nazwisko.text.toString()
+            if ( (dospr.text != "")  || (dospr2.text != ""))
             {
-                wyslane.setOnClickListener {
-                    val podane_imie = do_podania_imie.text.toString()
-                    val podane_nazwisko = do_podania_nazwisko.text.toString()
-                    tekst_wynik.text = "Podane Imie: " + podane_imie + "   Podane Nazwisko: " + podane_nazwisko
-                }
+                val podane_imie = do_podania_imie.text.toString()
+                val podane_nazwisko = do_podania_nazwisko.text.toString()
+                tekst_wynik.text = "Podane Imie: " + podane_imie + "   Podane Nazwisko: " + podane_nazwisko
             }
             else {
-                wyslane.setOnClickListener {
-                    tekst_wynik.text = "Wypełnij wszystkie pola"
-                }
-            }
+
+                tekst_wynik.text = "Wypełnij wszystkie pola"
             }
         }
+    }
+
+}
