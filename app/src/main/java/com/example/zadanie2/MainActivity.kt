1b3594f86ead2537ce2b95a88440864c6cce171f
Oskar <o_swiderski@o2.pl>
2021-11-10 08:11:21 +0100

    cale zadanie i jeden if nie dziala

--- app/src/main/java/com/example/zadanie2/MainActivity.kt
+++ app/src/main/java/com/example/zadanie2/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.zadanie2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-}
+
+        val wyslane = findViewById<Button>(R.id.wyslij)
+        val do_podania_imie = findViewById<TextInputEditText>(R.id.podane_imie)
+        val do_podania_nazwisko = findViewById<TextInputEditText>(R.id.podane_nazwisko)
+        val tekst_wynik = findViewById<TextView>(R.id.Wynik)
+
+            if (wyslane != null && do_podania_imie != null  && do_podania_nazwisko != null)
+            {
+                wyslane.setOnClickListener {
+                    val podane_imie = do_podania_imie.text.toString()
+                    val podane_nazwisko = do_podania_nazwisko.text.toString()
+                    tekst_wynik.text = "Podane Imie: " + podane_imie + "   Podane Nazwisko: " + podane_nazwisko
+                }
+            }
+            else {
+                wyslane.setOnClickListener {
+                    tekst_wynik.text = "Wypełnij wszystkie pola"
+                }
+            }
+            }
+        }
